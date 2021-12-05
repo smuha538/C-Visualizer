1341804fb40938894e5f48114d38d2362829819f
DarylleDiego <59937321+DarylleDiego@users.noreply.github.com>
2021-12-05 15:21:11 -0700

    created backup camera (#16)

--- CVisualizer/app/src/main/java/com/example/cvisualizer/MainActivity.java
+++ CVisualizer/app/src/main/java/com/example/cvisualizer/MainActivity.java
@@ -1,40 +1,65 @@
 package com.example.cvisualizer;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Color;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
-import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageView imageView;
-    private TextView textView;
-    private Bitmap bitmap;
-    //private Bitmap newImage;
-    int pixel;
+    private Button button;
+    private File photoFile;
+    private String FILE_NAME = "photo.jpg";
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.login_button) {
-            Intent log = new Intent(this, ImageEditActivity.class);
-            startActivity(log);
-        }
-    }
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.openCamera);
 
-        findViewById(R.id.login_button).setOnClickListener(this);
+        button.setOnClickListener(this);
+    }
 
+    public void onClick (final View v) {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            photoFile = getPhotoFile(FILE_NAME);
+            Uri fileProvider = FileProvider.getUriForFile(this, "com.example.fileprovider", photoFile);
+            takePicture.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+            startActivityForResult(takePicture, 100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private File getPhotoFile(String fileName) throws IOException {
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(fileName, ".jpg", storageDirectory);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+        imageView.setImageBitmap(photo);
     }
 }
